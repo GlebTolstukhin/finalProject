5020d791c1e6e4ba259365e12f6ced02b46485ba
Gleb Tolstukhin <132929424+GlebTolstukhin@users.noreply.github.com>
2024-03-21 16:09:41 +0600

    Online lesson commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+let x = 15
+let y = 20
+
+// 10 > 8 true
+// 10 > 11 false
+// 10 !== 10 false
+// 10 !== 11 true
+// 10 >= 9 true
+// 10 >= 10 true
+// 10 >= 11 false
+// <=
+if (x < y) {
+    console.log("Yes")
+    if (x === 15) {
+        console.log("x === 15")
+    }
+} else {
+    console.log("No")
+}
+
+
+console.log("done")
